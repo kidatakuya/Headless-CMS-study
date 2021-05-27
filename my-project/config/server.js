946ecf7e9ea3f9ba04ff86d50cbff3d79196f9f9
kidatakuya <kida.takuya.1234@gmail.com>
2021-05-27 21:57:50 +0900

    Environment

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fcf64f315658ea55bfdee2bfea094073'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f7724e8d-8529-4ac4-acd4-57cc39626543'
+};
